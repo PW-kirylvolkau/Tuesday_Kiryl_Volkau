456ec69a03e9e0a871bb54a75ecedc4f67f925cf
Kiryl Volkau <volkaukiryl@gmail.com>
2020-10-31 05:29:24 +0300

    L4: NameStep started.

--- lab4/src/components/CustomerForm/CustomerForm.tsx
+++ lab4/src/components/CustomerForm/CustomerForm.tsx
@@ -4,13 +4,24 @@ import AddressStep from "./AddressStep/AddressStep";
 import SummaryStep from "./SummaryStep/SummaryStep";
 
 const CustomerForm: React.FC<unknown> = () => {
+    // hooks for elements visibility
     const [address, setAddress] = useState<boolean>(false);
     const [summary, setSummary] = useState<boolean>(false);
+
+    // hooks for the state (data submitted in input
+    const [firstName, setFirstName] = useState<string>('');
+    const [lastName, setLastName] = useState<string>('');
+
 
     return(
         <div>
             <p>Customer form.</p>
-            <NameStep onClicked={() => setAddress(true)}/>
+            <NameStep
+                onClicked={() => setAddress(true)}
+                firstName={firstName}
+                onNameChange={(name:string) => setFirstName(name)}
+                lastName={lastName}
+            />
             {address && <AddressStep onClicked={() => setSummary(true)}/>}
             {summary && <SummaryStep />}
         </div>
--- lab4/src/components/CustomerForm/NameStep/NameStep.tsx
+++ lab4/src/components/CustomerForm/NameStep/NameStep.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 
-const NameStep: React.FC<{onClicked: () => void}> = ({onClicked}) => {
+interface NameStepProps {
+    onClicked: () => void,
+    onNameChange: (name: string) => void,
+    firstName: string,
+    lastName: string
+};
+
+const NameStep: React.FC<NameStepProps> = (props) => {
+    function isValid(): boolean {
+        if(props.firstName === '') {
+            return false;
+        }
+        return true;
+    }
 
     return(
         <div>
             <p>Name step.</p>
-            <button onClick={() => onClicked()}>Next</button>
+            <p>Name: {props.firstName}</p>
+            <input
+                type="text"
+                value={props.firstName}
+                onChange={(e) => props.onNameChange(e.target.value)}
+                placeholder="First name"
+            />
+            <br/>
+            <button
+                onClick={() => props.onClicked()}
+                disabled={!isValid()}
+            >
+                Next
+            </button>
         </div>
     );
 }
